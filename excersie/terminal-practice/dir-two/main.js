1d5e2626027a0fbb7555a75bb1a3022d80823dac
Adam Domnie <domniea@gmail.com>
2022-06-08 09:35:41 -0600

    master assignment folder

---
+++ excersie/terminal-practice/dir-two/main.js
@@ -0,0 +1,22 @@
+var button = document.getElementById("clickHere")
+
+function Hello() {
+	alert ("Hello World!")
+}
+
+button.addEventListener("click", function Hello() {
+	alert ("Hello World!")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
