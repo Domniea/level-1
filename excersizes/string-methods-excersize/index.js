89914eabc85451f415c27ced9402d53e7b136b1b
Adam Domnie <domniea@gmail.com>
2022-07-21 14:57:02 -0600

    Employee Records

--- excersizes/string-methods-excersize/index.js
+++ excersizes/string-methods-excersize/index.js
@@ -41,14 +41,57 @@ console.log(halfStrings("hello world"))
 // capilizeAndLowercase("Hello") // => "HEllo"
 // capilizeAndLowercase("Hello World") // => "HELLO world"
 
-function capitalsAndLowercase(string){
-    let arry = string.split("")
-    let half = Math.floor(arry.length / 2)
-    let arrBackHalf = arry.splice(half)
-    let frontHalf = arry.splice(half * (-1))
-    let stringFH = frontHalf.join("").toUpperCase()
-    let stringBH = arrBackHalf.join("").toLowerCase()
-    return stringFH + stringBH
+function capLow(string) {
+        let half = Math.floor(string.length / 2)
+        let fHalf = string.slice(0, half).toUpperCase()
+        let bHalf = string.slice(half).toLowerCase()
+        return fHalf + bHalf
+    }
+
+
+// console.log(capLow("hello world"))
+
+// function capitalsAndLowercase(string){
+//     let arry = string.split("")
+//     let half = Math.floor(arry.length / 2)
+//     let arrBackHalf = arry.splice(half)
+//     let frontHalf = arry.splice(half * (-1))
+//     let stringFH = frontHalf.join("").toUpperCase()
+//     let stringBH = arrBackHalf.join("").toLowerCase()
+//     return stringFH + stringBH
+// }
+
+// console.log(capitalsAndLowercase("balls balls"))
+
+// Write a function that takes a string as a parameter and capitalizes any character that follows a space.
+// capitalize("hey friends! practice practice practice!") // -> "Hey Friends! Practice Practice Practice!"
+
+
+function capitalize(string) {
+    string.toLowerCase()
+    let array = string.split(" ")
+    for (var i = 0; i < array.length; i++) {
+        array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1)
+    }
+    return array.join(" ")
+}
+console.log(capLow("hey there hello"))
+
+
+
+
+function Car(make, model, year){
+    this.make = make
+    this.model = model
+    this.year = year
 }
 
-console.log(capitalsAndLowercase("balls balls"))
+
+var jeep = new Car("Jeep", "Cherokee", 1995)
+var mazda = new Car("Mazda", "3 hatchback", 2015)
+ 
+console.log(mazda)
+ 
+
+
+
