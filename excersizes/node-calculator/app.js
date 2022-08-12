db914d4a0db7d2675f6dd9a8016241956c1cd2bb
Adam Domnie <domniea@gmail.com>
2022-08-12 12:27:19 -0600

    RPG

--- excersizes/node-calculator/app.js
+++ excersizes/node-calculator/app.js
@@ -1,10 +1,4 @@
 
-
-// let readlineSync = require('readline-sync'),
-//   animals = ['Lion', 'Elephant', 'Crocodile', 'Giraffe', 'Hippo'],
-//   index = readlineSync.keyInSelect(animals, 'Which animal?');
-// console.log('Ok, ' + animals[index] + ' goes to your room.');
-
 let readlineSync = require('readline-sync')
 
 function add(num1, num2) {
@@ -31,7 +25,7 @@ const functions = [
 ]
 
 let num1 = parseInt(readlineSync.question("Please choose the first number "))
-let num2 = parseInt(readlineSync.question("Please choose the second number "))2
+let num2 = parseInt(readlineSync.question("Please choose the second number "))
 operations = ["add", "subtract", "multiply", "divide"]
 let index = readlineSync.keyInSelect(operations, "Please choose an operation ") // enumerating the array for you
 
@@ -44,14 +38,12 @@ if (index === 1) subtract()
 if (index === 2) multiply
 
 if (index === 3) divide
-// if (1) {
-//   console.log(add(num1, num2))
-// }
-//  if (2) {
-//   console.log(subtract(num1, num2))
-// } if (3) {
-//   console.log(multiply(num1, num2))
-// } if (4) 
-//   console.log(divide(num1, num2))
-
-
+if (1) {
+  console.log(add(num1, num2))
+}
+ if (2) {
+  console.log(subtract(num1, num2))
+} if (3) {
+  console.log(multiply(num1, num2))
+} if (4) 
+  console.log(divide(num1, num2))
--- projects/rpg/app.js
+++ projects/rpg/app.js
@@ -1,5 +1,5 @@
-const readline= require('readline-sync')
- 
+const readline = require('readline-sync')
+
 mapActions = ['Walk']
 battleActions = ['Attack', 'Potion', 'Run']
 
@@ -10,31 +10,25 @@ inventory = {
 }
 
 heroIsAlive = true
-while (heroIsAlive === true) {
-    if (heroIsAlive === false) [
-        console.log("YOU'RE DEAD")
-    ]
-}
-
+enemyIsAlive = true
 enemy1HP = [50]
-
 
 function enemy1Atack() {
     let enemy1Damage = (Math.floor(Math.random() * 20))
-    console.log(enemy1Damage)
-    let newHp = characterHP[0] - enemy1Damage
-    characterHP.pop()
-    characterHP.push(newHp)
-    if (characterHP[0] <= 0) {
+    console.log(`${name} took ${enemy1Damage} of damage`)
+    let newHp = heroHP[0] - enemy1Damage
+    heroHP.pop()
+    heroHP.push(newHp)
+    if (heroHP[0] <= 0) {
         heroIsAlive = false
         console.log("Youre DEAD BUDDY!!!")
         }
-    console.log(characterHP)
+    console.log(`${name} is down to ${heroHP} HP`)
 }
 
 function heroAtack(string) {
     let heroDamage = (Math.floor(Math.random() * 20))
-    console.log(heroDamage)
+    console.log(`enemy1 took ${heroDamage} of damage`)
     let newHp = enemy1HP[0] - heroDamage
     enemy1HP.pop()
     enemy1HP.push(newHp)
@@ -42,7 +36,7 @@ function heroAtack(string) {
         enemyIsAlive = false
         console.log("You've vanquished the Enemy")
         }
-    console.log(enemyHP)
+    console.log(` Enemy is down to ${enemy1HP}`)
 }
 
 function potion(string) {
@@ -58,41 +52,60 @@ function run(string) {
         console.log("You ran away safely")
     }
     else {
-        console.log("The enemy blocked your path and you were unable to run away.")
+        console.log(`The enemy blocked ${name}'s path and you were unable to run away.`)
     }
 }
 
-function enemyAppearance() {
-    let enemyProbability = (Math.floor(Math.random() * 4)) 
-    if ((enemyProbability >= 3) && (heroIsAlive === true)) {
-        console.log("A monster has apeared in your path!")
-        console.log("What would you like to do?")
-        let move = readline.question("Type: attack, potion, or run")
-            if (move === "attack") {
-                heroAtack
+let enemy1Probability = (Math.floor(Math.random() * 10))
+
+function enemy1Appearance() {
+        console.log("A enemy1 has apeared in your path! ")
+        console.log("What would you like to do? ")
+        let move = readline.question("Type: attack, potion, or run ")
+        while (enemyIsAlive === true) {
+            if ((move === "attack") && (enemyIsAlive === true)) {
+                console.log(` ${name} atacked. `)
+                heroAtack()
+                break
             }
-            else if (move === "potion") {
+            if ((move === "potion")&& (enemyIsAlive === true)){
+                console.log(` ${name} used a potion. `)
                 potion()
-            } else if (move === "run") {
+                break
+            } 
+            if ((move === "run") && (enemyIsAlive === true)) {
+                console.log(`${name} escaped safeley. `)
                 run()
+                break
             }
-
+             enemy1Atack()
+            console.log("What would you like to do? ")
+            readline.question("Type: attack, potion, or run ")
+            }
+           
     }
-    else {
-        console.log("You made it a few strides further")
-    }
-}
 
-const name = readline.question('Welcome adventureist, what is your name? ')
+
+
+
+
+const name = readline.question('Welcome adventureist,lets first start my giving your Hero a name? ')
 console.log(`Welcome ${name}.`);
 console.log(`Welcome to the fighting arena`);
 
-const walk = readline.question('To walk forward type "walk" Give it a shot now. ')
-if (walk === 'walk') {
-    console.log('Good job! You swiftly took a few steps forward.')
+const firstStep = readline.question(`To walk forward type "walk" Give it a shot now. `)
+if (firstStep === 'walk') {
+    console.log(`Good job! ${name} swiftly took a few steps forward.`)
 }
-// let start = readline.question("Now that we've got that covered, lets begin. Start making your way to the tresure trove ")
 
-// if (walk === walk) {
-//     enemyAppearance()
-// }
+let walk = readline.question(`Now that we've got that covered, lets begin. Start making your way to the tresure trove. `)
+
+while (heroIsAlive === true) {
+ if ((walk === "walk") && (enemy1Probability > 3)) {
+    enemy1Appearance()
+}
+else {
+    console.log(`${name} walked down the path a bit more. `)
+}
+
+}
